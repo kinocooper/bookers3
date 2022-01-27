543d6248d39d8442c6041ffa17f2f64b4993c24a
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-27 02:10:04 +0000

    [add]いいね機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,6 @@ class BooksController < ApplicationController
   end
 
   def create
-    # @book = Book.new(book_params)
     @book = current_user.books.new(book_params)
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
