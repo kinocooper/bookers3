521e7a23b01bdffafb82631e65c4a1c79b590d6a
ShotaKinoshita <onque.unchain@gmail.com>
2022-01-27 02:39:39 +0000

    [fix]いいねリンク後の遷移先更新

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,13 +7,15 @@ class FavoritesController < ApplicationController
     # favorite.user_id = current_user.id
     # favorite.book_id = params[:book_id]
     favorite.save
-    redirect_to books_path
+    # redirect_to books_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     favorite = current_user.favorites.find_by(book_id: params[:book_id])
     favorite.destroy
-    redirect_to books_path
+    # redirect_to books_path
+    redirect_back(fallback_location: root_path)
   end
 
 end
