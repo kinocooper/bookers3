c7760270f8d14dc4e5493e46dd1ea2bd2362eace
ShotaKinoshita <onque.unchain@gmail.com>
2022-02-03 08:55:03 +0000

    relationshipのアクションルートをuserにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationship, only: [:create,:destroy]
+    get "followers_index" => "relationships#followers_index", as:"followers_index"
+    get "followeds_index" => "relationships#followeds_index", as:"followeds_index"
   end
 
-  get "relationship/followers_index/:user_id" => "relationships#followers_index", as:'followers_index'
-  get "relationship/followeds_index/:user_id" => "relationships#followeds_index", as:'followeds_index'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
