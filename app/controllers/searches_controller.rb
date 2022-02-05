936c80ba8b2ac5679e6944cf30968346f3a06aca
ShotaKinoshita <onque.unchain@gmail.com>
2022-02-05 01:58:41 +0000

    [add]検索機能…あいまい検索のみ

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,22 @@
 class SearchesController < ApplicationController
 
   def search
-    render '/homes/top'
+    @search_table = params[:search_table]
+    @search_words = params[:search_words]
+
+    # 検索対象がbooksか？
+    if @search_table == "books"
+      # booksテーブルのnameを検索にかけて条件に合うモデルを取得
+      @books = Book.where('title LIKE ?', "%#{params[:search_words]}%")
+      # (自動的にsearch.html.erbへrender)
+
+    # 検索対象がusersか？
+    elsif @search_table == "users"
+      # usersテーブルのtitleを検索にかけて条件に合うモデルを取得
+      @users = User.where('name LIKE ?', "%#{params[:search_words]}%")
+      # (自動的にsearch.html.erbへrender)
+    end
+
   end
 
 end
