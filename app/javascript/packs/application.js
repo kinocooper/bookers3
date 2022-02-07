8577a6f1848a26866db6f936cdd34232f42c652e
ShotaKinoshita <onque.unchain@gmail.com>
2022-02-07 08:22:38 +0000

    [add]JQuery使用環境

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,9 @@ import "channels"
 import "jquery"
 import "popper.js"
 import "bootstrap"
-import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import '@fortawesome/fontawesome-free/js/all'
+import "../stylesheets/application"
+import "JStest.js"
 
 Rails.start()
 Turbolinks.start()
